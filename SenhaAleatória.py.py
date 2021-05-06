a6fdd2ac0f2d88681a628f37bd99d16fdfd432f5
Gabriel Vitor da Silva <83681897+GvitorSilva@users.noreply.github.com>
2021-05-06 10:40:37 -0300

    Add files via upload

---
+++ SenhaAleatória.py.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[35]:
+
+
+from random import sample
+low = 'abcdefghijklmnopqrstuvwxyz'
+upp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+num = '0123456789'
+sym = '!@#$^&*'
+
+todos = low + upp + num + sym
+comprimento = 10
+senha = ''. join (sample(todos, comprimento))
+imprimir = (senha)
+
+print(f'Sua senha é {imprimir}')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
